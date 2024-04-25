189f4ab348ea7ccd4e2e9880dd2968be9f42ccb0
GreviVillegas <villegasgrevi@gmail.com>
2024-04-25 04:24:13 -0400

    Create DatoXY.java

---
+++ Implementación_pt2/DatoXY.java
@@ -0,0 +1,28 @@
+
+public class DatoXY{
+    private int x;
+    private int y;
+    public DatoXY(){
+        x = 0;
+        y = 0;
+    }
+
+    public DatoXY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+}
