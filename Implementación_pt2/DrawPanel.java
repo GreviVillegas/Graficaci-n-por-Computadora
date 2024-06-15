77cb8812b12e05bd00ffe08a8f5b7a3d277060f9
GreviVillegas <villegasgrevi@gmail.com>
2024-06-15 02:23:41 -0400

    Update DrawPanel.java

--- Implementación_pt2/DrawPanel.java
+++ Implementación_pt2/DrawPanel.java
@@ -2,22 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DrawPanel extends JPanel {
-    private Circunferencia circunferencia;
-    private static final int GRID_SIZE = 10; // Tamaño de la grilla en píxeles
-    private int grosorLinea = 1;
-    public void setCircunferencia(Circunferencia circunferencia) {
-        this.circunferencia = circunferencia;
+    private Triangulo triangulo;
+
+    public void setTriangulo(Triangulo triangulo) {
+        this.triangulo = triangulo;
     }
 
-    public Circunferencia getCircunferencia() {
-        return circunferencia;
+    public Triangulo getTriangulo() {
+        return triangulo;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Dibujar la grilla
+        /* 
         g.setColor(Color.LIGHT_GRAY);
         for (int i = 0; i <= this.getWidth(); i += GRID_SIZE) {
             g.drawLine(i, 0, i, this.getHeight());
@@ -25,18 +24,45 @@ public class DrawPanel extends JPanel {
         for (int i = 0; i <= this.getHeight(); i += GRID_SIZE) {
             g.drawLine(0, i, this.getWidth(), i);
         }
+        */
+        Color co = g.getColor();
+        g.setColor(Color.WHITE);
+        dibujarBorde(g, 2);
+        dibujarBorde(g, 10);
+        g.setColor(co);
+        
+        if(triangulo != null){
+            int xCuarto = this.getWidth() / 4;
+            int yCuarto = this.getHeight() / 4;
+            int ymasMitad = ((this.getHeight() / 2) + (this.getHeight() / 4));
+            int xmasMitad = ((this.getWidth() / 2) + (this.getWidth() / 4));
+            int xMedio = (this.getWidth()/2);
+            int tamaño = triangulo.getTamaño();
 
-        // Dibujar la circunferencia
-        if (circunferencia != null) {
-            g.setColor(Color.BLACK);
-            int centerX = this.getWidth() / 2;
-            int centerY = this.getHeight() / 2;
-            circunferencia.dibujar(g);
+            DatoXY p1 = new DatoXY(xCuarto - tamaño, ymasMitad + tamaño);
+            DatoXY p2 = new DatoXY(xmasMitad + tamaño, ymasMitad + tamaño);
+            DatoXY p3 = new DatoXY(xMedio, yCuarto - tamaño);
+            triangulo.setDatosTriangulo(p1, p2, p3);
+            triangulo.dibujar(g);
         }
+        
+        // Dibujar el triangulo
+
+        /*
+         * if (circunferencia != null) {
+         * g.setColor(Color.BLACK);
+         * int centerX = this.getWidth() / 2;
+         * int centerY = this.getHeight() / 2;
+         * circunferencia.dibujar(g);
+         * }
+         */
     }
 
-    
-public void setLineThickness(int gl) {
-    this.grosorLinea = gl;
-}
+    private void dibujarBorde(Graphics g, int gri){
+        g.drawLine(gri, gri, this.getWidth()-gri, gri);
+        g.drawLine(gri, gri, gri, this.getHeight()-gri);
+        g.drawLine(gri, this.getHeight()-gri, this.getWidth()-gri, this.getHeight()-gri);
+        g.drawLine(this.getWidth()-gri, this.getHeight()-gri, this.getWidth()-gri, gri);
+    }
+
 }
